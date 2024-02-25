4df12098a0f5453d7b514bb9df4905817540442e
Yoee <Yoee_Chan@163.com>
2024-02-25 15:25:26 +0800

    Init Projectsss

---
+++ Event-Bin/src/event/repository/Repository.java
@@ -0,0 +1,7 @@
+package event.repository;
+import event.repository.entity.ColumnType;
+import event.repository.entity.Guid;
+public class Repository {
+    private  String eventName;
+
+}
---
+++ Event-Bin/src/event/repository/entity/ColumnType.java
@@ -0,0 +1,4 @@
+package event.repository.entity;
+
+public class ColumnType {
+}
